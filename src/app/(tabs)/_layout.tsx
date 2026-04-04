9ac97d8f52d88cca549611a52130e9c83f9bb3be
Vitor <vitor.camargo.ofc@gmail.com>
2026-04-04 13:59:40 -0300

    🚧Att: reestuturação passes

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -46,7 +46,7 @@ export default function Layout() {
         name="passes"
         options={{
           title: "Passes",
-          headerShown: true,
+          headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="ticket" size={size} color={color} />
           ),
