00e62048bfacbbf0fa25af08742735154ed0725e
Vitor <vitor.camargo.ofc@gmail.com>
2026-04-05 00:36:54 -0300

    ✨Feat: mais imagens para tela inicial!

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,53 @@
 import { Link } from "expo-router";
+import React, { useEffect, useRef } from "react";
 import {
   ImageBackground,
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
   View,
+  Dimensions,
 } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+const { width } = Dimensions.get("window");
+
 export default function Index() {
+  const scrollRef= useRef<ScrollView>(null);
+
+  useEffect(() => {
+    let indice = 0;
+    const tempo = setInterval(() => {
+      indice = indice === 2 ? 0 : indice + 1;
+      scrollRef.current?.scrollTo({ x: indice * width, animated: true });
+    }, 5000);
+
+    return () => clearInterval(tempo);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-        <ImageBackground
-          source={require("@/assets/fotoPonte.jpeg")}
-          style={styles.image}
+        <ScrollView
+          ref={scrollRef}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          scrollEnabled={false}
         >
+          <ImageBackground source={require("@/assets/fotoPonte1.jpeg")} style={{ width, height: "100%" }} />
+          <ImageBackground source={require("@/assets/fotoPonte2.jpg")} style={{ width, height: "100%" }} />
+          <ImageBackground source={require("@/assets/fotoPonte3.jpg")} style={{ width, height: "100%" }} />
+        </ScrollView>
+
+        <View style={styles.botaoContainer}>
           <Link href="/atracoes" asChild>
             <Pressable style={styles.blockButton}>
               <Text style={styles.text}>Bem-Vindo ao FloriPasse</Text>
             </Pressable>
           </Link>
-        </ImageBackground>
+        </View>
       </View>
     </SafeAreaProvider>
   );
@@ -32,10 +58,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignContent: "center",
   },
-  image: {
-    flex: 1,
-    justifyContent: "flex-start",
-    resizeMode: "cover",
+  botaoContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-start"
   },
   text: {
     color: "#E7DBD9",
